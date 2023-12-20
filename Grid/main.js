7d7d9c25d7b785ea766e2d71d9c2cea898078815
Junko <phuhaon394@gmail.com>
2023-12-20 10:00:51 +0700

    feat: add javaScript

--- Grid/main.js
+++ Grid/main.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded" ,function(){
-    const galleryPictures = document.querySelectorAll(".gallery__picture");   
-    galleryPictures.forEach(function(picture){
-        picture.addEventListener("click",function(){
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryPictures = document.querySelectorAll(".gallery__picture");
+    galleryPictures.forEach(function (picture) {
+        picture.addEventListener("click", function () {
             console.log("Image clicked:", picture);
         });
     });
