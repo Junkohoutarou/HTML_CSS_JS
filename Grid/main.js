2749eccb06762cd4664c01446b9abe866756c29c
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-24 23:40:09 +0700

    Update main.js

--- Grid/main.js
+++ Grid/main.js
@@ -1,1 +1,9 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryPictures = document.querySelectorAll(".gallery__picture");
+    galleryPictures.forEach(function (picture) {
+        picture.addEventListener("click", function () {
+            console.log("Image clicked:", picture);
+        });
+    });
+});
